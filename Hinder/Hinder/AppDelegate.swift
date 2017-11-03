53e616d72fd5fe4f433f08b176ecf47e8cbe2808
kimsvatos <kimsvatos@ucla.edu>
2017-11-03 15:32:33 -0700

    first commit, add xCode skeleton

---
+++ Hinder/Hinder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hinder
+//
+//  Created by Kim Svatos on 11/3/17.
+//  Copyright © 2017 TBD. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
