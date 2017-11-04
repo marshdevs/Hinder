1beffb08dcd3e46e88c6ea405960283e19b2f708
kimsvatos <kimsvatos@ucla.edu>
2017-11-03 17:38:11 -0700

    add Pods and Podfile

--- Hinder/Hinder/AppDelegate.swift
+++ Hinder/Hinder/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     window = UIWindow(frame: UIScreen.main.bounds)
-    window?.backgroundColor = UIColor.black
+    window?.backgroundColor = UIColor.white
     let nav = UINavigationController(navigationBarClass: CustomNavigationBar.self, toolbarClass: nil)
     nav.pushViewController(FeedViewController(), animated: false)
     window?.rootViewController = nav
