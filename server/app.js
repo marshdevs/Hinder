b0d15e6030829fa53db74e76bca8038d61f0d9f4
Marshall Briggs <marsh@Marshalls-MacBook-Pro.local>
2017-11-03 17:38:37 -0700

    App.js skeleton (create&delete events, users, and projects, and match), requests.json is our sample data schema

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ var aws = require('aws-sdk');
 var app = express()
 
 var region = "us-west";
-var accessKeyId = "AKIAJUWZLWURXRRFR7OQ";
-var secretAccessKey = "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";
+var accessKeyId = "AKIAJ266NZR4QF75FE3A";
+var secretAccessKey = "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";
 
 aws.config.update({
     region: region,
@@ -25,24 +25,27 @@ app.post('/createEvent', function(req, res){
 
     req.on('data', function(data){
         var event = JSON.parse(data.toString());
-        
+        var eventId = generateEID();
         var params = {
             TableName = "hinder-events";
             Item: {
-                // Theoretical event schema
-                "eventId": generateId(),
+                "eventId": eventId,
                 "name": event.name,
                 "date": event.date,
                 "location": event.location,
                 "description": event.description,
                 "photo": event.photo,
-                "thumbnail": event.thumbnail
+                "thumbnail": event.thumbnail,
+                "projects": event.projects,
+                "users": event.users
             }
         };
         dynamoDB.putItem(params, function(err, data){
             if (err) {
                 console.log(err);
-                console.log("Error creating event: " + event);
+                console.log("Error creating event: " + eventId + " - " + event.name);
+            } else {
+                console.log("Successfully created event: " + eventId + " - " + event.name);
             }
         });
     });
@@ -61,15 +64,15 @@ app.delete('/deleteEvent', function(req, res){
         var params = {
             TableName: "hinder-events";
             Key: {
-                eventId: event.id
+                eventId: event.eventId
             }
         };
         dynamoDB.deleteItem(params, function(err, data){
             if (err) {
                 console.log(err);
-                console.log();
+                console.log("Error deleting event: " + event.eventId + " - " + event.name);
             } else {
-                console.log("Successfully deleted event: " + event.id + " - " + event.name);
+                console.log("Successfully deleted event: " + event.eventId + " - " + event.name);
             }
         });
 
@@ -81,6 +84,29 @@ app.delete('/deleteEvent', function(req, res){
 app.post('/createUser', function(req, res){
     console.log("Received a createUser request...");
 
+    req.on('data', function(data){
+        var user = JSON.parse(data.toString());
+        var userId = generateUID();
+        var params = {
+            TableName = "hinder-users";
+            Item: {
+                "userId": userId,
+                "name": user.name,
+                "occupation": user.occupation,
+                "photo": user.photo,
+                "events": user.events,
+                "skills": user.skills
+            }
+        };
+        dynamoDB.putItem(params, function(err, data){
+            if (err) {
+                console.log(err);
+                console.log("Error creating user: " + userId + " - " + user.name);
+            } else {
+                console.log("Successfully created user: " + userId + " - " + user.name);
+            }
+        });
+    });
 });
 
 app.put('/editUser', function(req, res){
@@ -91,6 +117,24 @@ app.put('/editUser', function(req, res){
 app.delete('/deleteUser', function(req, res){
     console.log("Received a deleteUser request...");
 
+    req.on('data', function(data){
+        var user = JSON.parse(data.toString());
+        var params = {
+            TableName: "hinder-users";
+            Key: {
+                userId: user.userId
+            }
+        };
+        dynamoDB.deleteItem(params, function(err, data){
+            if (err) {
+                console.log(err);
+                console.log("Error deleting user: " + user.userId + " - " + user.name);
+            } else {
+                console.log("Successfully deleted user: " + user.userId + " - " + user.name);
+            }
+        });
+
+    });
 });
 
 // Project operations ---------------------------------
@@ -98,6 +142,31 @@ app.delete('/deleteUser', function(req, res){
 app.post('/createProject', function(req, res){
     console.log("Received a createProject request...");
 
+    req.on('data', function(data){
+        var project = JSON.parse(data.toString());
+        var projectId = generatePID();
+        var params = {
+            TableName = "hinder-projects";
+            Item: {
+                "projectId": projectId,
+                "eventId": project.eventId,
+                "name": project.name,
+                "description": project.description,
+                "size": project.size,
+                "photo": project.photo,
+                "skills": project.skills,
+                "users": project.users
+            }
+        };
+        dynamoDB.putItem(params, function(err, data){
+            if (err) {
+                console.log(err);
+                console.log("Error creating project: " + projectId + " - " + project.name);
+            } else {
+                console.log("Successfully created project: " + projectId + " - " + project.name);
+            }
+        });
+    });
 });
 
 app.put('/editProject', function(req, res){
@@ -108,11 +177,47 @@ app.put('/editProject', function(req, res){
 app.delete('/deleteProject', function(req, res){
     console.log("Received a deleteProject request...");
 
+    req.on('data', function(data){
+        var project = JSON.parse(data.toString());
+        var params = {
+            TableName: "hinder-projects";
+            Key: {
+                projectId: user.projectId
+            }
+        };
+        dynamoDB.deleteItem(params, function(err, data){
+            if (err) {
+                console.log(err);
+                console.log("Error deleting project: " + project.projectId + " - " + project.name);
+            } else {
+                console.log("Successfully deleted project: " + project.projectId + " - " + project.name);
+            }
+        });
+
+    });
 });
 
 // Matching operations -----------------------------------
 
-app.put('/', function(req, res){
-    console.log("Received a  request...");
+app.put('/match', function(req, res){
+    console.log("Received a match request...");
 
+    req.on('data', function(data){
+        var match = JSON.parse(data.toString());
+        var params = {
+            TableName = "hinder-matches";
+            Item: {
+                "matchId": match.matchId,
+                "matched": match.matched
+            }
+        };
+        dynamoDB.putItem(params, function(err, data){
+            if (err) {
+                console.log(err);
+                console.log("Error creating match: " + matchId);
+            } else {
+                console.log("Successfully created match: " + projectId + " - " + match.matched);
+            }
+        });
+    });
 });
